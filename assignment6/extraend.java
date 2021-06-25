22ba6426d1c126e18610c16e38a39e02f055fef8
manojvsm <58743587+manojvsm@users.noreply.github.com>
2021-06-25 15:16:06 +0530

    Update extraend.java

--- assignment6/extraend.java
+++ assignment6/extraend.java
@@ -11,3 +11,39 @@ extraEnd("Hi") "HiHiHi"
 */
 
 
+import java.util.Scanner;
+public class assignment1 {
+	
+	static void extraend(String str)
+	{
+		String strl;
+		int strlen=str.length();
+		if(strlen<=1)
+			System.out.println("enter the atleast two length of string");
+		else
+		{	strl=str.substring(strlen-2);
+			for(int i=0;i<3;i++)	
+				System.out.print(strl);
+		}
+	}
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the string");
+		String str=sc.nextLine();
+		extraend(str);
+	}
+}
+/*
+output
+
+enter the string
+hghgc
+gcgcgc
+
+enter the string
+hello
+lololo
+
+
+*/
