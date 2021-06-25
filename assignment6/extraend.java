5f25756c12fe16ed961324c7415dbcf3b0fd1c26
manojvsm <58743587+manojvsm@users.noreply.github.com>
2021-06-25 15:13:49 +0530

    Create extraend.java

---
+++ assignment6/extraend.java
@@ -0,0 +1,13 @@
+/*
+Given a string, return a new string made of 3 copies of the last 2 chars of the original string. The string length will be atleast 2.
+
+extraEnd("Hello") "lololo"
+
+extraEnd("ab") → "ababab"
+
+extraEnd("Hi") "HiHiHi"
+
+
+*/
+
+
